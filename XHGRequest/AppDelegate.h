2fac3365137d262a7ecb3be790399599ba4b1499
kaxi <kaxi@kaxidemacbook-air.local>
2016-05-26 12:01:54 +0800

    Initial Commit

---
+++ XHGRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XHGRequest
+//
+//  Created by kaxi on 16/5/26.
+//  Copyright © 2016年 kaxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XHGRequest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XHGRequest
+//
+//  Created by kaxi on 16/5/26.
+//  Copyright © 2016年 kaxi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
